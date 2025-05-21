1d75db5841b1204ddee10d0d9ee021754fe8aed7
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-05-21 21:37:37 +1000

    chore: linting

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
+import initState from './initState'
 import parseTemplate from './parseTemplate'
 import processTemplateMacros from './processTemplateMarcos'
 import setupArrowFunctionHandler from './setupArrowFunctionHandler'
 import triggerDomUpdates from './triggerDomUpdates'
-import initState from './initState'
 
 export default {
 	parseTemplate,
